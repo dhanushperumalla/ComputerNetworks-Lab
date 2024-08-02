fac74a688b61f7b0d5af4a737da01d081c6079b0
Perumalla Venkata Naga Dhansush <118119552+dhanushperumalla@users.noreply.github.com>
2024-08-01 23:23:15 -0700

    Upload CheckSum

---
+++ CHECKSUM.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+int sender(int arr[10],int n) {
+  int checksum, sum=0,i;
+  printf("\n****SENDER SIDE****\n");
+  for (i=0;i<n;i++)  sum+=arr[i];
+  printf("SUM IS: %d",sum);
+  checksum=~sum; //1's complement of sum
+  printf("\nCHECKSUM IS:%d",checksum);
+  return checksum;
+  }
+  void receiver(int arr[10],int n,int sch){
+    int checksum, sum=0,i;
+    printf ("\n\n****RECEIVER SIDE****\n");
+    for(i=0;i<n;i++)
+    sum+=arr[i];
+    printf("SUM IS:%d",sum);
+    sum=sum+sch;
+    checksum=~sum;
+    printf("\nCHECKSUM IS:%d",checksum);
+   } void main() {
+   int n,i,sch,rch,arr[50];
+   clrscr();
+   printf("\nENTER SIZE OF THE STRING:");
+   scanf("%d",&n);
+   printf("ENTER THE ELEMENTS OF THE ARRAY TO CALCULATE CHECKSUM:\n");
+   for(i=0;i<n;i++)  {
+   scanf("%d",&arr[i]);
+     }
+       sch=sender(arr,n);
+     receiver(arr,n,sch);
+      getch();
+       }
